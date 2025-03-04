03b7d6712c5c229f395dea68527e786a471ae325
catalina-acosta <catalina.acosta.rivera@gmail.com>
2025-03-04 12:50:01 +0100

    added onSnapshot and unsubList and unsubSingle

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { AsyncPipe } from '@angular/common';
 import { inject } from '@angular/core';
-import { Firestore, collection, doc, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
 import { Note } from '../interfaces/note.interface';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,20 +14,47 @@ export class NoteListService {
   normalNotes: Note[] = [];
 
   items$;
+  items;
+  
+  unsubList;
+  unsubSingle;
+
   firestore = inject(Firestore);
   
+  constructor() { 
+    this.unsubList = onSnapshot(this.getNotesRef(),  (list) => {
+      list.forEach(element => {
+        console.log(element);
+        
+      })
+    });
+
+    this.unsubSingle = onSnapshot(this.getSingleDoc("notes", "jahdisohf"),  (element) => {
+      console.log(element);
+      
+    });
+
+    this.unsubSingle(); // equivallent of unsubscribe
+    this.unsubList(); 
+
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list) => {
+      list.forEach(element => {
+        console.log(element);
+        
+      })
+    })
+  }
+
   getTrashReft() {
     return collection(this.firestore, 'trash');
   }
   
-  getNotesReft() {
+  getNotesRef() {
     return collection(this.firestore, 'notes');
   }
   
   getSingleDoc(colId:string, docId:string) {
     return doc(collection(this.firestore, colId), docId);
-  }
-  constructor() { 
-    items$ = collectionData(this.getNotesReft())
   }
 }
